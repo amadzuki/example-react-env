e45709a7166fd42db200608dc0d8e7af4fd78685
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-04-28 16:56:21 +0700

    Use environment variable for request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+import request from "./utils/request"
 
 const App = () => {
-  return <div>App</div>
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    request.get("users").then((response) => {
+      setUsers(response.data)
+    })
+  })
+  return (
+    <div>
+      <h1>Example React Env</h1>
+      <ul>
+        {users.length > 0 &&
+          users.map((user) => {
+            return <li>{user.login}</li>
+          })}
+      </ul>
+    </div>
+  )
 }
 
 export default App
